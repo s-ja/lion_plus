4f138052060528679ae812af4f596a43290ed1d3
s-ja <dkstmdwl0615@naver.com>
2023-10-24 15:51:28 +0900

    object like array

--- workspace/ch01/ex01-13.js
+++ workspace/ch01/ex01-13.js
@@ -1,25 +1,30 @@
 // 지정한 배열의 모든 요소를 출력한다.
-function printArr(arr){
-  for(var i=0; i<arr.length; i++){
+function printArr(arr) {
+  for (var i = 0; i < arr.length; i++) {
     console.log(i, arr[i]);
   }
 }
 
 // 지정한 객체의 모든 속성을 출력한다.
-function printObj(obj){
-  
+function printObj(obj) {
+  for (var prop in obj) {
+    console.log(prop, obj[prop]);
+  }
 }
 
-var arr = ['zero', 'one', 'two'];
-arr.push('three');
-arr.push('four');
+var arr = ["zero", "one", "two"];
+arr.push("three");
+arr.push("four");
 arr.pop();
 arr.shift();
 
 // 일반 객체처럼 속성 추가
+arr.name = "asj";
 
 // 대입연산자로 속성 추가. enumerable, writable, configurable true로 설정됨
+arr["age"] = 29;
 
 // 속성 추가 전용 메소드
 
 printArr(arr);
+printObj(arr);
