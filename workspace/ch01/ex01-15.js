4c2b8d3c0f51e96803dc149d11636c3965090f29
s-ja <dkstmdwl0615@naver.com>
2023-10-24 16:37:19 +0900

    function practice

--- workspace/ch01/ex01-15.js
+++ workspace/ch01/ex01-15.js
@@ -1,11 +1,20 @@
-var arr = ['zero', 'one', 'three', 'four'];
+var arr = ["zero", "one", "three", "four"];
 
-console.log('arr', arr);
+var arr2 = arr.splice(2, 0, "two");
+
+console.log("arr", arr);
+
+console.log("arr2", arr2);
 
 // splice(start[, deleteCount[, item1[, item2[, ...]]]])
 // 배열의 요소를 삭제, 교체, 추가하고 제거한 요소를 담은 배열을 반환한다.
 // deleteCount 생략시 마지막까지 제거
 
+// var arr2 = arr.splice(3, 1);
+
+// var arr2 = arr.splice(1);
+
+// var arr2 = arr.splice();
 
 // slice([begin[, end]])
 // 배열의 begin부터 end까지(end 미포함) 요소를 복사해서 새로운 배열을 반환한다.
@@ -14,7 +23,12 @@ console.log('arr', arr);
 // end 생략시 마지막까지 복사
 // 원본 데이터 유지
 
+var arr2 = arr.slice(1, 3);
+var arr2 = arr.slice(-3, -1);
+var arr2 = arr.slice(2);
+var arr2 = arr.slice(-2);
+var arr2 = arr.slice(); // 전체복사
 
-console.log('arr', arr);
-console.log('arr2', arr2);
+console.log("arr", arr);
+console.log("arr2", arr2);
 console.log(arr == arr2);
--- workspace/ch01/ex01-16.js
+++ workspace/ch01/ex01-16.js
@@ -1,19 +1,45 @@
 // 10 + 100의 결과를 출력한다.
-function add(){
+// function add() {
+//   return 10 + 100;
+// }
 
+function add() {
+  let n1 = 10;
+  let n2 = 100;
+  let result = n1 + n2;
+
+  console.log(`${n1} + ${n2} = ${result}`);
 }
 
 // 전달받은 숫자와 100의 합계를 출력한다.
-function add100(n1){
+// function add100(n1) {
+//   return n1 + 100;
+// }
 
+function add100(n1) {
+  let n2 = 100;
+  let result = n1 + n2;
+  console.log(`${n1} + ${n2} = ${result}`);
 }
 
 // 전달받은 두 수의 합계를 출력한다.
-function sum(n1, n2){
-  
+// function sum(n1, n2) {
+//   return n1 + n2;
+// }
+
+function sum(n1, n2) {
+  let result = n1 + n2;
+  return `${n1}+${n2}=${result}`;
 }
 
 // 전달받은 두 수의 합계를 반환한다.
-function sum2(n1, n2){
-
+function sum2(n1, n2) {
+  return n1 + n2;
 }
+
+console.log(add());
+add();
+add100(20);
+add100(30);
+console.log(sum(10, 20));
+console.log(sum2(20, 30));
