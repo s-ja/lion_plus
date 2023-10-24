103ec28b463b94452f511746d3f74da328457474
Kilyong Jeong <uzoolove@gmail.com>
2023-10-24 14:00:19 +0900

    .

--- workspace-ins/ch01/ex01-04.js
+++ workspace-ins/ch01/ex01-04.js
@@ -10,4 +10,16 @@ console.log(s1 === s3);
 console.log(s1, s2, s3);
 
 // Symbol.for(): 자바스크립트 엔진이 관리하는 전역 심볼 레지스트리에 저장됨
+var obj = {
+  'hello': 10,
+  'world': 20
+};
+obj.hello = 30;
+console.log(obj);
 
+var obj2 = {
+  [Symbol('hello')]: 10,
+  [Symbol('world')]: 20,
+};
+obj2[Symbol('hello')] = 30;
+console.log(obj2);
