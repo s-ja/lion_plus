c08e7aca5ec82b2c5149f9f86aaacf02a16e9eb1
Kilyong Jeong <uzoolove@gmail.com>
2023-10-25 12:45:39 +0900

    .

--- sample/day2/ch02/ex02-20.js
+++ sample/day2/ch02/ex02-20.js
@@ -21,10 +21,6 @@ var list = [
   }
 ]
 
-// sort() _id로 내림차순 정렬
-var todolist = [];
-console.log('정렬 결과', todolist);
-
 // forEach() 완료된 할일 목록
 var doneList = [];
 console.log('forEach() 완료된 할일 목록', doneList);
