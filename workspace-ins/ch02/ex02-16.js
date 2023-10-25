63836c9121a2bbe2fffad74e52a34b36d8b0e28f
Kilyong Jeong <uzoolove@gmail.com>
2023-10-25 16:54:01 +0900

    .

--- workspace-ins/ch02/ex02-16.js
+++ workspace-ins/ch02/ex02-16.js
@@ -4,7 +4,12 @@ console.log(Math.min(100,20,60,50,70)); // 20
 
 // 지정한 배열의 요소 중 최소값을 반환
 function smallest(nums){
-	
+	var min = nums[0];
+  for(var i=1; i<nums.length; i++){
+    if(nums[i] < min) min = nums[i];
+    // min = Math.min(min, nums[i]);
+  }
+  return min;
 }
 
 console.log(smallest([10,100]));	// 10
