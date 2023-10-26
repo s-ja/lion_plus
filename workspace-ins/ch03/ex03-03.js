02a727a09e344d2a2940dfbcb968f2fc9da53a34
Kilyong Jeong <uzoolove@gmail.com>
2023-10-26 14:32:21 +0900

    .

--- workspace-ins/ch03/ex03-03.js
+++ workspace-ins/ch03/ex03-03.js
@@ -1,6 +1,12 @@
 // prototype 이용해서 작성(ex03-01.js 파일 참조)
 var Person = function(name){
-  
+  this.name = name;
+};
+Person.prototype.getName = function(){
+  return this.name;
+};
+Person.prototype.setName = function(name){
+  this.name = name;
 };
 
 var kim = new Person('김철수');
@@ -11,5 +17,5 @@ console.log(lee.getName());	// 이영희
 kim.setName('홍길동');
 console.log(kim.getName());	// 홍길동
 
-console.log(kim.getName == lee.getName);	// true
-console.log(kim.setName == lee.setName);	// true
+console.log(kim.getName === lee.getName);	// true
+console.log(kim.setName === lee.setName);	// true
--- workspace-ins/ch03/ex03-04.js
+++ workspace-ins/ch03/ex03-04.js
@@ -1,6 +1,6 @@
 // 고등학교 성적관리 생성자 함수(총점과 평균 계산)
 function HighSchool(kor, eng){
-
+  
 }
 
 var s1 = new HighSchool(100, 91);
