4f03e9db2b36203c4d5ef697b8a50bc5369afb61
s-ja <dkstmdwl0615@naver.com>
2023-10-27 16:28:32 +0900

    async practice

--- workspace/ch05/ex05-03.js
+++ workspace/ch05/ex05-03.js
@@ -1,19 +1,44 @@
-function syncFn1() {
-  console.log("syncFn1 작업 시작");
+// function syncFn1() {
+//   console.log("syncFn1 작업 시작");
 
-  console.log("syncFn1 작업 종료");
-  return "syncFn1 result";
+//   console.log("syncFn1 작업 종료");
+//   return "syncFn1 result";
+// }
+
+// function syncFn2(f1Result) {
+//   console.log("syncFn2 작업 시작", f1Result);
+
+//   console.log("syncFn2 작업 종료");
+//   return "syncFn2 result";
+// }
+
+// (function () {
+//   const result = syncFn1();
+//   syncFn2(result);
+//   console.log("test completed");
+// })();
+
+function asyncFn1(resolve) {
+  console.log("asyncFn1 work start");
+  setTimeout(function () {
+    console.log("asyncFn1 work complete");
+    resolve("asyncFn1 result");
+  }, 3000);
 }
 
-function syncFn2(f1Result) {
-  console.log("syncFn2 작업 시작", f1Result);
-
-  console.log("syncFn2 작업 종료");
-  return "syncFn2 result";
+function asyncFn2(f1Result, resolve) {
+  console.log("asyncFn2 work start", f1Result);
+  setTimeout(function () {
+    console.log("asyncFn2 work complete");
+    resolve("asyncFn2 result");
+  }, 2000);
 }
 
 (function () {
-  const result = syncFn1();
-  syncFn2(result);
+  asyncFn1(function (result) {
+    asyncFn2(result, function (f2result) {
+      console.log(f2result);
+    });
+  });
   console.log("test completed");
 })();
